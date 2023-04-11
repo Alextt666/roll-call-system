1134ef655c3a1970fb1f266fc9d617f5d0f23056
alexzhang <alexzhangyaotian@gmail.com>
2023-04-11 12:39:07 +0800

    add:班级切换

--- roll-call/src/components/DataStruct/AllData.vue
+++ roll-call/src/components/DataStruct/AllData.vue
@@ -1,9 +1,28 @@
 <template>
   <div class="class-items">
-    <div class="class-item">
-      艳自拔小学三年五班
-    </div>
+    <template v-for="(item,index) in schoolList">
+      <div :class="['class-item', selectedSchool == index ? 'class-item-active':'']" @click="schoolChangeEffect(item,index)">艳自拔小学三年五班</div></template
+    >
   </div>
 </template>
-<script setup></script>
-<style></style>
+<script setup>
+import {reactive,ref} from 'vue';
+const schoolList = reactive([1,2,3]);
+let selectedSchool = ref(999)
+function schoolChangeEffect(item,index){
+  selectedSchool.value = index;
+}
+</script>
+<style scoped lang="scss">
+.class-items {
+  border: 1px solid black;
+  display: flex;
+  justify-content: space-around;
+  cursor: pointer;
+  color: $text-gray;
+}
+.class-item-active{
+  color: $text-dark;
+  border-bottom: 1px solid $text-dark;
+}
+</style>
