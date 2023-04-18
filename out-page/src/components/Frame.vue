4710adca394bebcf46a30b763bed744d4fc2b15e
alexzhang <alexzhangyaotian@gmail.com>
2023-04-18 13:38:27 +0800

    modify:iframe权限

--- out-page/src/components/Frame.vue
+++ out-page/src/components/Frame.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <iframe class="frame-wrap" :src="classUrl" frameborder="0"> </iframe>
+    <iframe class="frame-wrap" :src="classUrl" frameborder="0" allowfullscreen="true"> </iframe>
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref,onMounted } from "vue";
 import { fetchClassUrl } from "@/api/request/index.js";
 const classUrl = ref("");
 const timeTableId = window.location.search.split("=")[1];
@@ -13,6 +13,11 @@ async function getClassUrl(timeTableId) {
   classUrl.value = res?.url ? res.url : "http://www.bing.com";
 }
 getClassUrl(timeTableId);
+onMounted(()=>{
+  const frame = document.querySelector('iframe');
+  console.log(frame)
+  frame.allow="camera *; microphone *; autoplay *; midi *; encrypted-media *;"
+})
 </script>
 <style scoped>
 .frame-wrap {
